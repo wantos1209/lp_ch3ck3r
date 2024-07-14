c2399cf9d9189a3d5d14d273d1966f822c85dc9b
Wantos <irwanzzz65@gmail.com>
2024-07-15 04:10:17 +0700

    update

--- resources/views/scan.blade.php
+++ resources/views/scan.blade.php
@@ -5,7 +5,14 @@
     <title>Barcode Scanner</title>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/quagga"></script>
-    
+    <style>
+        #yourScannerDiv {
+            width: 100%; /* lebar sesuai dengan keinginan Anda */
+            height: 400px; /* tinggi sesuai dengan keinginan Anda */
+            margin: auto; /* menengahkan div */
+            border: 1px solid #ccc; /* garis pinggir untuk membedakan area pemindaian */
+        }
+    </style>
 </head>
 
 <body>
