2d95c1c312ed978ac0f910698f2843d7fcc518c4
Wantos <irwanzzz65@gmail.com>
2024-07-18 01:25:03 +0700

    update

--- resources/views/barcode.blade.php
+++ resources/views/barcode.blade.php
@@ -4,19 +4,36 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Barcode Scanner</title>
-    {{-- <script src="https://cdnjs.cloudflare.com/ajax/libs/quagga/0.12.1/quagga.min.js"></script> --}}
-    <script src="https://unpkg.com/html5-qrcode@2.0.9/dist/html5-qrcode.min.js"></script>
-
+    <script src="https://unpkg.com/html5-qrcode/minified/html5-qrcode.min.js"></script>
 </head>
 <body>
     <h1>Barcode Scanner</h1>
-    <div id="qr-reader" style="width: 600px"></div>
+    <div id="reader" style="width: 600px;"></div>
     <script>
         function onScanSuccess(decodedText, decodedResult) {
-        alert(`Code scanned = ${decodedText}`, decodedResult);
-}
-        var html5QrcodeScanner = new Html5QrcodeScanner(
-            "qr-reader", { fps: 10, qrbox: 250 });
+            alert(`Code scanned = ${decodedText}`, decodedResult);
+
+            // Kirim barcode ke server Laravel
+            // fetch('{{ route('barcode.scan') }}', {
+            //     method: 'POST',
+            //     headers: {
+            //         'Content-Type': 'application/json',
+            //         'X-CSRF-TOKEN': '{{ csrf_token() }}'
+            //     },
+            //     body: JSON.stringify({ barcode: decodedText })
+            // })
+            // .then(response => response.json())
+            // .then(data => {
+            //     console.log('Success:', data);
+            //     // Tampilkan data produk atau informasi lainnya di sini
+            // })
+            // .catch((error) => {
+            //     console.error('Error:', error);
+            // });
+        }
+
+        let html5QrcodeScanner = new Html5QrcodeScanner(
+            "reader", { fps: 10, qrbox: 250 });
         html5QrcodeScanner.render(onScanSuccess);
     </script>
 </body>
