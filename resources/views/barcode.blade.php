5da49ccaa608d6d5d088efbc6cf40912710192f6
Wantos <irwanzzz65@gmail.com>
2024-07-18 01:29:06 +0700

    update

--- resources/views/barcode.blade.php
+++ resources/views/barcode.blade.php
@@ -11,30 +11,34 @@
     <div id="reader" style="width: 600px;"></div>
     <script>
         function onScanSuccess(decodedText, decodedResult) {
-            alert(`Code scanned = ${decodedText}`, decodedResult);
+            console.log(`Code scanned = ${decodedText}`, decodedResult);
 
             // Kirim barcode ke server Laravel
-            // fetch('{{ route('barcode.scan') }}', {
-            //     method: 'POST',
-            //     headers: {
-            //         'Content-Type': 'application/json',
-            //         'X-CSRF-TOKEN': '{{ csrf_token() }}'
-            //     },
-            //     body: JSON.stringify({ barcode: decodedText })
-            // })
-            // .then(response => response.json())
-            // .then(data => {
-            //     console.log('Success:', data);
-            //     // Tampilkan data produk atau informasi lainnya di sini
-            // })
-            // .catch((error) => {
-            //     console.error('Error:', error);
-            // });
+            fetch('{{ route('barcode.scan') }}', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'X-CSRF-TOKEN': '{{ csrf_token() }}'
+                },
+                body: JSON.stringify({ barcode: decodedText })
+            })
+            .then(response => response.json())
+            .then(data => {
+                console.log('Success:', data);
+                // Tampilkan data produk atau informasi lainnya di sini
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+            });
+        }
+
+        function onScanFailure(error) {
+            console.warn(`QR error = ${error}`);
         }
 
         let html5QrcodeScanner = new Html5QrcodeScanner(
             "reader", { fps: 10, qrbox: 250 });
-        html5QrcodeScanner.render(onScanSuccess);
+        html5QrcodeScanner.render(onScanSuccess, onScanFailure);
     </script>
 </body>
 </html>
